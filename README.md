439b2fbe061424ebf21c1f1d0fe6d3a9f332a303
mdetweiler <mdetweil@gmail.com>
2021-05-03 15:53:42 -0500

    First push

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # url_shortener
+
+to run:
+`docker-compose up --scale api=2`
+
+frontend located:
+`localhost:8080`
+
+api located:
+`localhost:8001`
---
+++ frontend/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  {{ result }}
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup() {
+    const result = ref("")
+
+    onMounted(async () => {
+      try {
+        const response = axios.get('http://localhost:8001/example')
+        console.log(response);
+      } catch(e) {
+        //result.value = e;
+      }
+    })
+
+    return {
+      result
+    }
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
