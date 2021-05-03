778635ff185c89c910d6d9a8ceb9569cc3271bef
mdetweiler <mdetweil@gmail.com>
2021-05-03 17:38:20 -0500

    Away from vite

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,39 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  {{ result }}
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue'
+import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
-import axios from 'axios'
 
 export default defineComponent({
   name: 'App',
   components: {
     HelloWorld
-  },
-  setup() {
-    const result = ref("")
-
-    onMounted(async () => {
-      try {
-        const response = axios.get('http://localhost:8001/example')
-        console.log(response);
-      } catch(e) {
-        //result.value = e;
-      }
-    })
-
-    return {
-      result
-    }
   }
 })
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
