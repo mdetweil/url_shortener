641bdce0a6c4c027443f2103a925ac7d6add0e32
mdetweiler <mdetweil@gmail.com>
2021-05-03 21:36:21 -0500

    add redis pt 1

--- api/routes/example/index.js
+++ api/routes/example/index.js
@@ -1,7 +1,14 @@
-'use strict'
+"use strict";
 
 module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    return 'this is an example'
-  })
-}
+  fastify.get("/", async function (request, reply) {
+    const { redis } = fastify;
+    redis.set("hello", "world", (err) => {
+      console.log("error", err);
+    });
+
+    redis.get("hello", (err, val) => {
+      reply.send(err || val);
+    });
+  });
+};
