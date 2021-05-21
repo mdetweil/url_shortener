9218e0be0c503c0db991bfc8ac0724d898fc3047
mdetweiler <mdetweil@gmail.com>
2021-05-20 21:28:27 -0500

    add tailwind

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    {{ query }}
+  <div class="bg-red-400 h-screen">
+    <div class="container mx-auto pt-6">
+      {{ query }}
+    </div>
   </div>
 </template>
 
@@ -30,12 +32,4 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
